52411ba873d8d2e5d88f7ec1f22d36bc87313307
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-14 22:44:58 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,7 @@
 magic_exit_number = 1
-count = 0
-while count >= 10 do
+count = 10
+while count > 0 do
   break if count == magic_exit_number
   puts "Happy New Year!"
-  count -= 1
+  count = count - 1
 end
